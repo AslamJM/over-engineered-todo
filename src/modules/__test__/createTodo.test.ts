953810740a2ba3f5625809027b3bd642fd9c6b32
AslamJM <jma8744@gmail.com>
2022-08-21 03:20:03 +0530

    feat: adds swagger docs

---
+++ src/modules/__test__/createTodo.test.ts
@@ -0,0 +1,37 @@
+import { nanoid } from 'nanoid';
+import { describe, it, vi, expect } from 'vitest';
+import { createServer } from '../../utils/createServer';
+import * as TodoService from '../todos.service';
+
+describe('POST "/api/todos" route', () => {
+  it('should call the createTodo service', async () => {
+    const todo = {
+      _id: 'mock id',
+      title: 'mock title',
+      shortId: nanoid(),
+      completed: false,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+
+    const createTodoSpy = vi.spyOn(TodoService, 'createTodo');
+    expect(createTodoSpy.getMockName()).toEqual('createTodo');
+    createTodoSpy.mockResolvedValue(todo);
+
+    const server = await createServer();
+    await server.ready();
+
+    const payload = {
+      title: 'A test todo',
+    };
+
+    const response = await server.inject({
+      method: 'POST',
+      url: '/api/todos',
+      payload,
+    });
+
+    expect(response.json()).toEqual(todo);
+    expect(createTodoSpy).toHaveBeenCalledWith(payload);
+  });
+});
--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -1,7 +1,24 @@
 import fastify from 'fastify';
+import { todoRoutes } from '../modules/todo.routes';
+import swagger from '@fastify/swagger';
+import { version } from '../../package.json';
 
 export async function createServer() {
   const app = fastify();
+  app.register(todoRoutes, { prefix: '/api/todos' });
+  app.register(swagger, {
+    routePrefix: '/docs',
+    swagger: {
+      tags: [{ name: 'todo' }],
+      info: {
+        title: 'Todo',
+        description: 'A simple todo app',
+        version,
+      },
+    },
+    staticCSP: true,
+    exposeRoute: true,
+  });
 
   return app;
 }
